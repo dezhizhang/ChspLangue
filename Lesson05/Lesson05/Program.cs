046ec9debf76ee7fd3a6f15e7a14438e645fc10b
dezhizhang <1541609448@qq.com>
2025-06-29 19:28:08 +0800

    feat: 更新题目

---
+++ Lesson05/Lesson05/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Lesson05
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string userName = "tom";
+            string password = "123";
+            Console.WriteLine($"姓名:{userName} 密码:{password}");
+        }
+    }
+}
