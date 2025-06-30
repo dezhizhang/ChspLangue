5e1861a1ee9b64d37dd66079a5a4fbd2fbc243da
dezhizhang <1541609448@qq.com>
2025-06-30 22:32:55 +0800

    feat: arrayList

--- Lesson14/Lesson14/Program.cs
+++ Lesson14/Lesson14/Program.cs
@@ -1,12 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Collections;
+
 namespace Lesson14
 {
     class Program
     {
-        public void Main(string[] args)
+        public static void Main(string[] args)
         {
-            
+            ArrayList list = new ArrayList();
+            list.Add("A");
+            list.Add("B");
+
+            list.Add("F");
+            list.Add("C");
+            list.Add("D");
+            list.Add("E");
+
+            list.Sort();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
         }
     }
 }
